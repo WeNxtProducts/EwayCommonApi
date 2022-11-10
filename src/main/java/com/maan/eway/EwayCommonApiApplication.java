a3e68d98bab22cec896d77967ae35046e3b3cd01
Samilla <89253083+MaanRsa001@users.noreply.github.com>
2022-11-10 13:28:25 +0530

    New Common Source Created

---
+++ src/main/java/com/maan/eway/EwayCommonApiApplication.java
@@ -0,0 +1,13 @@
+package com.maan.eway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EwayCommonApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EwayCommonApiApplication.class, args);
+	}
+
+}
