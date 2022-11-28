52dfa6e28501ac99b8322550e6e4dd35938a2097
Kumar <89253083+MaanRsa001@users.noreply.github.com>
2022-11-28 17:13:29 +0530

    War Rate Master Completed

---
+++ src/main/java/com/maan/eway/master/req/WarRateMasterSaveReq.java
@@ -0,0 +1,61 @@
+package com.maan.eway.master.req;
+
+import java.util.Date;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import lombok.Data;
+
+@Data
+public class WarRateMasterSaveReq {
+
+	@JsonProperty("WarRateId")
+	private String warRateId;
+	
+	@JsonProperty("WarRateDesc")
+	private String warRateDesc;
+	
+	@JsonProperty("WarRate")
+	private String warRate;
+	
+	@JsonProperty("ModeTransportId")
+	private String modeTransportId;
+	
+	@JsonProperty("BranchCode")
+	private String branchCode;
+	
+	@JsonProperty("InsuranceId")
+	private String companyId;
+	
+	@JsonFormat(pattern="dd/MM/yyyy")
+	@JsonProperty("EffectiveDateStart")
+	private Date effectiveDateStart;
+	
+	@JsonFormat(pattern="dd/MM/yyyy")
+	@JsonProperty("EffectiveDateEnd")
+	private Date effectiveDateEnd;
+	
+	@JsonProperty("Remarks")
+	private String remarks;
+	
+	@JsonProperty("Status")
+	private String status;
+	
+	@JsonProperty("CreatedBy")
+	private String createdBy;
+	
+	@JsonProperty("UpdatedBy")
+	private String updatedBy;
+	
+	@JsonFormat(pattern="dd/MM/yyyy")
+	@JsonProperty("UpdatedDate")
+	private Date updatedDate;
+
+	@JsonProperty("CoreAppCode")
+	private String coreAppCode;
+	
+	@JsonProperty("RegulatoryCode")
+	private String regulatoryCode;
+	
+}
