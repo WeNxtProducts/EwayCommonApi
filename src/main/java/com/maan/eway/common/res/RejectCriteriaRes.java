3ec1555faa9d09368f21fb479b3ee20c9b2eead7
Samilla <89253083+MaanRsa001@users.noreply.github.com>
2022-12-15 19:01:13 +0530

    Update RejectCriteriaRes.java

--- src/main/java/com/maan/eway/common/res/RejectCriteriaRes.java
+++ src/main/java/com/maan/eway/common/res/RejectCriteriaRes.java
@@ -4,11 +4,14 @@ import java.util.Date;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+@Getter
+@Setter
 @NoArgsConstructor
 @AllArgsConstructor
-@Data
 public class RejectCriteriaRes {
 
 	// Customer Info
