cae18a87f8d8a4806749b6dc2f85ee3041b90c5f
Suganya <89253083+MaanRsa001@users.noreply.github.com>
2022-12-26 12:54:20 +0530

    Update EserviceCustomerDetailsServiceImpl.java

--- src/main/java/com/maan/eway/common/service/impl/EserviceCustomerDetailsServiceImpl.java
+++ src/main/java/com/maan/eway/common/service/impl/EserviceCustomerDetailsServiceImpl.java
@@ -95,7 +95,7 @@ public class EserviceCustomerDetailsServiceImpl implements EserviceCustomerDetai
 				} else if (req.getClientName().length() > 100) {
 					errorList.add(new Error("01", "ClientName", "Please Enter ClientName within 100 Characters"));
 				} 
-				else if (StringUtils.isNotBlank(req.getClientName())&& !req.getClientName().matches("^[A-Za-z]")) {
+				else if (StringUtils.isNotBlank(req.getClientName())&& !req.getClientName().matches("(?i)[a-z]([- ',.a-z]{0,23}[a-z])?")) {
 					errorList.add(new Error("01", "ClientName", "Please Enter Proper ClientName"));						
 				}
 				
