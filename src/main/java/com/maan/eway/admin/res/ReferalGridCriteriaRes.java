acc3cca12223db4141a7ec5876d39ceec89e034d
Samilla <89253083+MaanRsa001@users.noreply.github.com>
2022-12-26 17:57:52 +0530

    Grid Changes

--- src/main/java/com/maan/eway/admin/res/ReferalGridCriteriaRes.java
+++ src/main/java/com/maan/eway/admin/res/ReferalGridCriteriaRes.java
@@ -2,9 +2,16 @@ package com.maan.eway.admin.res;
 
 import java.util.Date;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
-@Data
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class ReferalGridCriteriaRes {
 
 
