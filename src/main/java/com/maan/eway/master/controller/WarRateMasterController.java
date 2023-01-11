01fccfed831f7711057c54c2b596b6175f1ae58e
Suganya <89253083+MaanRsa001@users.noreply.github.com>
2023-01-11 10:26:19 +0530

    Update WarRateMasterController.java

--- src/main/java/com/maan/eway/master/controller/WarRateMasterController.java
+++ src/main/java/com/maan/eway/master/controller/WarRateMasterController.java
@@ -104,7 +104,7 @@ public ResponseEntity<CommonRes> getallWarRate(@RequestBody WarRateMasterGetallR
 
 //  Get Active War RAte Master
 
-	@PostMapping("/getallwarrate")
+	@PostMapping("/getactivewarrate")
 	@ApiOperation("This method is get Active War Rate")
 	public ResponseEntity<CommonRes> getActiveWarrate(@RequestBody WarRateMasterGetallReq req)
 	{
