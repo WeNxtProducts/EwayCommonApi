a5f145764bb30ab927a9728b26597e22b9266aff
Kumar <89253083+MaanRsa001@users.noreply.github.com>
2023-01-12 13:03:12 +0530

    Payment

--- src/main/java/com/maan/eway/bean/PaymentDetail.java
+++ src/main/java/com/maan/eway/bean/PaymentDetail.java
@@ -15,7 +15,6 @@ package com.maan.eway.bean;
 
 import org.hibernate.annotations.DynamicInsert;
 import org.hibernate.annotations.DynamicUpdate;
-import javax.persistence.Table;
 
 import lombok.*;
 import javax.persistence.*;
@@ -197,6 +196,17 @@ private static final long serialVersionUID = 1L;
     @Column(name="UPDATED_BY", length=100)
     private String     updatedBy;
 
+    @Column(name="EMI_YN", length=100)
+    private String     emiYn ;
+    
+    @Column(name="INSTALLMENT_MONTH", length=20)
+    private String     installmentMonth;
+    
+    @Column(name="INSTALLMENT_PERIOD", length=20)
+    private String     installmentPeriod;
+
+    
+    
 }
 
 
--- src/main/java/com/maan/eway/common/req/PaymentDetailsSaveReq.java
+++ src/main/java/com/maan/eway/common/req/PaymentDetailsSaveReq.java
@@ -27,4 +27,8 @@ public class PaymentDetailsSaveReq {
 	
 	@JsonProperty("InsuranceId")
 	private String insuranceId;
+	
+	@JsonProperty("EmiYn")
+	private String emiYn;
+
 }
