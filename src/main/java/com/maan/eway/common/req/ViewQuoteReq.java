798370fe1c1619fcab624fdce630a2c81b717af9
Samilla <89253083+MaanRsa001@users.noreply.github.com>
2023-01-13 14:39:46 +0530

    QuoteNO Changes

--- src/main/java/com/maan/eway/common/req/ViewQuoteReq.java
+++ src/main/java/com/maan/eway/common/req/ViewQuoteReq.java
@@ -4,9 +4,11 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 
 import lombok.Builder;
 import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 
-@Data
-@Builder
+@Getter
+@Setter
 public class ViewQuoteReq {
 
 	@JsonProperty("QuoteNo")
