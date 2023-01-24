246b4127436ee1b6f462d6b5724c053a2a6787e9
Suganya <89253083+MaanRsa001@users.noreply.github.com>
2023-01-24 17:46:31 +0530

    Update ReferalGridCriteriaRes.java

--- src/main/java/com/maan/eway/admin/res/ReferalGridCriteriaRes.java
+++ src/main/java/com/maan/eway/admin/res/ReferalGridCriteriaRes.java
@@ -35,6 +35,6 @@ public class ReferalGridCriteriaRes {
 	private Date policyEndDate;
 	private String rejectReason;
 	private String adminRemarks;
-	private String referalRemarks;
+	private Date entryDate;
 
 }
