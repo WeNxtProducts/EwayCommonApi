3aedfdb78def811bedd671c562d981308ee3936c
Samilla <89253083+MaanRsa001@users.noreply.github.com>
2023-01-30 16:56:29 +0530

    Update LoginProductServiceImpl.java

--- src/main/java/com/maan/eway/admin/service/impl/LoginProductServiceImpl.java
+++ src/main/java/com/maan/eway/admin/service/impl/LoginProductServiceImpl.java
@@ -336,24 +336,13 @@ public class LoginProductServiceImpl  implements LoginProductService {
 			
 			
 			// Effective Date Max Filter
-			Subquery<Long> effectiveDate = query.subquery(Long.class);
-			Root<LoginProductMaster> ocpm1 = effectiveDate.from(LoginProductMaster.class);
-			effectiveDate.select(cb.max(ocpm1.get("effectiveDateStart")));
+			Subquery<Long> amendId = query.subquery(Long.class);
+			Root<LoginProductMaster> ocpm1 = amendId.from(LoginProductMaster.class);
+			amendId.select(cb.max(ocpm1.get("amendId")));
 			Predicate a1 = cb.equal(c.get("productId"),ocpm1.get("productId") );
 			Predicate a2 = cb.equal(c.get("companyId"),ocpm1.get("companyId") );
-			Predicate a3 = cb.lessThanOrEqualTo(ocpm1.get("effectiveDateStart"), today);
-			Predicate a4 = cb.equal(c.get("loginId"),ocpm1.get("loginId") );
-			effectiveDate.where(a1,a2,a3,a4);
-			
-			// Effective Date Max Filter
-			Subquery<Long> effectiveDate2 = query.subquery(Long.class);
-			Root<LoginProductMaster> ocpm2 = effectiveDate2.from(LoginProductMaster.class);
-			effectiveDate2.select(cb.max(ocpm2.get("effectiveDateEnd")));
-			Predicate a5 = cb.equal(c.get("productId"),ocpm2.get("productId") );
-			Predicate a6 = cb.equal(c.get("companyId"),ocpm2.get("companyId") );
-			Predicate a7 = cb.greaterThanOrEqualTo(ocpm2.get("effectiveDateEnd"), todayEnd);
-			Predicate a8 = cb.equal(c.get("loginId"),ocpm2.get("loginId") );
-			effectiveDate2.where(a5,a6,a7,a8);
+			Predicate a3 = cb.equal(c.get("loginId"),ocpm1.get("loginId") );
+			amendId.where(a1,a2,a3);
 			
 			// Filer Product IDs
 			Subquery<Long> productIds = query.subquery(Long.class);
@@ -388,13 +377,11 @@ public class LoginProductServiceImpl  implements LoginProductService {
 			Expression<String>e0=c.get("productId");
 			
 		    // Where	
-			Predicate n1 = cb.equal(c.get("status"), "Y");
-			Predicate n2 = cb.equal(c.get("effectiveDateStart"), effectiveDate);
-			Predicate n3 = cb.equal(c.get("effectiveDateEnd"), effectiveDate2);
+			Predicate n2 = cb.equal(c.get("amendId"), amendId);
 			Predicate n4 = cb.equal(c.get("companyId"), companyIds.get(0));
 			Predicate n5 = cb.equal(c.get("loginId"), loginId);
 			Predicate n6 = e0.in(productIds);
-			query.where(n1,n2,n3,n4,n5,n6).orderBy(orderList);
+			query.where(n2,n4,n5,n6).orderBy(orderList);
 			
 			// Get Result
 			TypedQuery<LoginProductMaster> result = em.createQuery(query);			
