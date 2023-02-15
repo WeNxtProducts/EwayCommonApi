52f2ab1efa511f76822b0f7329ff7b0c66efdbb8
Nagarajan <maansarovarwork@gmail.com>
2023-02-15 16:27:49 +0530

    building changes

--- src/main/java/com/maan/eway/bean/EserviceBuildingDetails.java
+++ src/main/java/com/maan/eway/bean/EserviceBuildingDetails.java
@@ -355,8 +355,6 @@ private static final long serialVersionUID = 1L;
       @Column(name="INDUSTRY_DESC", length=100)
     private String  industryDesc ;
 
-      @Column(name="WORKMEN_COMP_SUMINSURED")
-      private BigDecimal     workmenCompSuminsured;
       
 }
 
