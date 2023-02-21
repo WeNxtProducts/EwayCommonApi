5541cd0b255a421bd28bf9095e999a29ba974ded
Nagarajan <maansarovarwork@gmail.com>
2023-02-21 17:06:32 +0530

    issue

--- src/main/java/com/maan/eway/notification/service/NotificationService.java
+++ src/main/java/com/maan/eway/notification/service/NotificationService.java
@@ -99,7 +99,7 @@ public class NotificationService {
 					.otp(n.getOtp())
 					.policyNo(n.getPolicyNo())
 					.quoteNo(n.getQuoteNo()) 
-					.uwMailid(n.getUnderwriters().subList(0, 5).stream().map(a -> a.getUwMailid()).collect(Collectors.joining(",")))
+					.uwMailid(n.getUnderwriters().size()>5?n.getUnderwriters().subList(0, 5).stream().map(a -> a.getUwMailid()).collect(Collectors.joining(",")):n.getUnderwriters().stream().map(a -> a.getUwMailid()).collect(Collectors.joining(","))
 					.uwMessengerCode(n.getUnderwriters().get(0).getUwMessengerCode())
 					.uwMessengerPhone(n.getUnderwriters().get(0).getUwMessengerPhone())
 					.uwName(n.getUnderwriters().get(0).getUwName())
