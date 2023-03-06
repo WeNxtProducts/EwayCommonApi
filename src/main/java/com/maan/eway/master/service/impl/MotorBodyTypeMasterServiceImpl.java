fd6f3df69e7298829de7acda70803563632a1a38
Suganya <89253083+MaanRsa001@users.noreply.github.com>
2023-03-06 16:46:59 +0530

    updates

--- src/main/java/com/maan/eway/master/service/impl/MotorBodyTypeMasterServiceImpl.java
+++ src/main/java/com/maan/eway/master/service/impl/MotorBodyTypeMasterServiceImpl.java
@@ -103,15 +103,15 @@ public class MotorBodyTypeMasterServiceImpl implements MotorBodyTypeMasterServic
 				errorList
 						.add(new Error("02", "EffectiveDateStart", "Please Enter Effective Date Start as Future Date"));
 			}
-			//Status Validation
+			// Status Validation
 			if (StringUtils.isBlank(req.getStatus())) {
-			errorList.add(new Error("05", "Status", "Please Select Status  "));
+				errorList.add(new Error("05", "Status", "Please Enter Status"));
 			} else if (req.getStatus().length() > 1) {
-			errorList.add(new Error("05", "Status", "Please Select Valid Status - 1 Character Only Allwed"));
-			}else if(!("Y".equalsIgnoreCase(req.getStatus())||"N".equalsIgnoreCase(req.getStatus())||"R".equalsIgnoreCase(req.getStatus())|| "P".equalsIgnoreCase(req.getStatus()))) {
-			errorList.add(new Error("05", "Status", "Please Select Valid Status - Active or Deactive or Pending or Referral "));
+				errorList.add(new Error("05", "Status", "Enter Status in One Character Only"));
+			} else if (!("Y".equals(req.getStatus()) || "N".equals(req.getStatus()) || "R".equals(req.getStatus())
+					|| "P".equals(req.getStatus()))) {
+				errorList.add(new Error("05", "Status", "Please Enter Status "));
 			}
-	
 			if (StringUtils.isBlank(req.getCompanyId())) {
 				errorList.add(new Error("04", "CompanyId", "Please Enter CompanyId"));
 			} else if (req.getCompanyId().length() > 20) {
