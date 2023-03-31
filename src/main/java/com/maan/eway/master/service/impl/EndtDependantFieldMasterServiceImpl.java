9a1d0d25ab39cf979ed30cc262c1d6cee6ba2854
Kumar <89253083+MaanRsa001@users.noreply.github.com>
2023-03-31 17:51:31 +0530

    endt

--- src/main/java/com/maan/eway/master/service/impl/EndtDependantFieldMasterServiceImpl.java
+++ src/main/java/com/maan/eway/master/service/impl/EndtDependantFieldMasterServiceImpl.java
@@ -376,11 +376,8 @@ public class EndtDependantFieldMasterServiceImpl implements EndtDependantFieldMa
 			Predicate n1 = cb.equal(b.get("amendId"), amendId);
 			Predicate n2 = cb.equal(b.get("companyId"), req.getCompanyId());
 			Predicate n3 = cb.equal(b.get("productId"),req.getProductId());
-			Predicate n4 = cb.equal(b.get("status"),"Y");
-			Predicate n5 = cb.equal(b.get("status"),"R");
-			Predicate n6 = cb.or(n4,n5);
 			
-			query.where(n1,n2,n3,n6).orderBy(orderList);
+			query.where(n1,n2,n3).orderBy(orderList);
 			
 			
 			/*
