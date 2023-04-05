15e6592ca7acc75f64925936988084974d066d58
Shanish <maanrsa001@gmail.com>
2023-04-05 15:51:11 +0530

    endt issue

--- src/main/java/com/maan/eway/calculator/util/EndtCoverCalculator.java
+++ src/main/java/com/maan/eway/calculator/util/EndtCoverCalculator.java
@@ -75,7 +75,7 @@ public class EndtCoverCalculator  extends CommonCalculator implements Consumer<C
 					List<Tuple> prorata =  crservice.loadProRataData(engine, diff);	
 					if(prorata.size()>0) {
 					 BigDecimal percenat=prorata.get(0).get("percent")==null?BigDecimal.ZERO:new BigDecimal(prorata.get(0).get("percent").toString());	
-					 t.setProRata(new BigDecimal("100").subtract(percenat).divide(new BigDecimal("100")));
+					 t.setProRata(percenat.divide(new BigDecimal("100")));
 					}else {
 						t.setProRata(new BigDecimal("1"));
 					}
