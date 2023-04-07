972c79572f76532edb545ed2c829380ed4deb465
Nandhini <maanrsa001@gmail.com>
2023-04-07 19:21:17 +0530

    Model Update

--- src/main/java/com/maan/eway/master/service/impl/MotorMakeModelMasterServiceImpl.java
+++ src/main/java/com/maan/eway/master/service/impl/MotorMakeModelMasterServiceImpl.java
@@ -448,10 +448,10 @@ public class MotorMakeModelMasterServiceImpl implements MotorMakeModelMasterServ
 			Predicate n1 = cb.equal(b.get("amendId"), amendId);
 			Predicate n2 = cb.equal(b.get("companyId"), req.getInsuranceId());
 			Predicate n3 = cb.equal(b.get("branchCode"), req.getBranchCode());
-//			Predicate n4 = cb.equal(b.get("branchCode"), "99999");
-//			Predicate n5 = cb.or(n3,n4);
+			Predicate n4 = cb.equal(b.get("branchCode"), "99999");
+			Predicate n5 = cb.or(n3,n4);
 			Predicate n6 = cb.equal(b.get("makeId"), req.getMakeId());
-			query.where(n1,n2,n3,n6).orderBy(orderList);
+			query.where(n1,n2,n5,n6).orderBy(orderList);
 			
 			// Get Result
 			TypedQuery<MotorMakeModelMaster> result = em.createQuery(query);
