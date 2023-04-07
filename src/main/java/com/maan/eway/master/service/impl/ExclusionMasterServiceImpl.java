5bb3bdeccbb10435d864b06b76e2ad0d7e57dbe3
Kumar <89253083+MaanRsa001@users.noreply.github.com>
2023-04-07 16:18:07 +0530

    branch code

--- src/main/java/com/maan/eway/master/service/impl/ExclusionMasterServiceImpl.java
+++ src/main/java/com/maan/eway/master/service/impl/ExclusionMasterServiceImpl.java
@@ -405,14 +405,14 @@ public List<ExclusionMasterRes> getallExclusion(ExclusionMasterGetallReq req) {
 				Predicate n1 = cb.equal(b.get("amendId"), amendId);
 				Predicate n2 = cb.equal(b.get("companyId"), req.getCompanyId());
 				Predicate n3 = cb.equal(b.get("branchCode"), req.getBranchCode());
-				Predicate n4 = cb.equal(b.get("branchCode"),"99999");
-				Predicate n5 = cb.or(n3,n4);
+			//	Predicate n4 = cb.equal(b.get("branchCode"),"99999");
+			//	Predicate n5 = cb.or(n3,n4);
 				Predicate n6 = cb.equal(b.get("productId"), req.getProductId());
 				Predicate n9 = cb.equal(b.get("sectionId"), req.getSectionId());
 				Predicate n10 = cb.equal(b.get("sectionId"),"99999");
 				Predicate n11 = cb.or(n9,n10);
 				
-				query.where(n1,n2,n5,n6,n11).orderBy(orderList);
+				query.where(n1,n2,n3,n6,n11).orderBy(orderList);
 		
 		// Get Result
 		TypedQuery<ExclusionMaster> result = em.createQuery(query);
