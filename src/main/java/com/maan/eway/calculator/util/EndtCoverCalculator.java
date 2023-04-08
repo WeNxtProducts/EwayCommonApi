497f059cc473dea652d5ed6e3a73310d57c5da21
Shanish <maanrsa001@gmail.com>
2023-04-08 17:11:44 +0530

    endt issue
    calc

--- src/main/java/com/maan/eway/calculator/util/EndtCoverCalculator.java
+++ src/main/java/com/maan/eway/calculator/util/EndtCoverCalculator.java
@@ -55,7 +55,7 @@ public class EndtCoverCalculator  extends CommonCalculator implements Consumer<C
 				 
 				 t.setProRata(new BigDecimal("1"));
 				 
-				 if(prorata!=null && prorata.size()>0 && "Y".equals(t.getProRataYn()) && !"Y".equals(t.getUserOpt())) {
+				 if(prorata!=null && prorata.size()>0 && "Y".equals(t.getProRataYn()) ) {
 					 BigDecimal percenat=prorata.get(0).get("percent")==null?BigDecimal.ZERO:new BigDecimal(prorata.get(0).get("percent").toString());	
 					 t.setProRata(percenat.divide(new BigDecimal("100")));
 				 }
