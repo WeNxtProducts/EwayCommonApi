3e979964ecaf190ad575b34125c6d3679074a8b7
Nagarajan <maansarovarwork@gmail.com>
2023-04-13 06:57:51 +0530

    Update SearchService.java

--- src/main/java/com/maan/eway/common/service/SearchService.java
+++ src/main/java/com/maan/eway/common/service/SearchService.java
@@ -1,34 +1,19 @@
 package com.maan.eway.common.service;
 
 import java.util.List;
-import com.maan.eway.error.Error;
-import com.maan.eway.common.req.CopyQuoteReq;
-import com.maan.eway.common.req.DocumentReq;
-import com.maan.eway.common.req.ExistingQuoteReq;
-import com.maan.eway.common.req.IssuerQuoteReq;
-import com.maan.eway.common.req.PaymentInformationGetReq;
+
 import com.maan.eway.common.req.SearchEservieMotorDetailsViewRatingRes;
 import com.maan.eway.common.req.SearchReq;
-import com.maan.eway.common.req.UpdateLapsedQuoteReq;
-import com.maan.eway.common.req.ViewQuoteReq;
 import com.maan.eway.common.res.AdminViewQuoteRes;
-import com.maan.eway.common.res.CommonRes;
 import com.maan.eway.common.res.DocumentRes;
-import com.maan.eway.common.res.EserviceCustomerDetailsRes;
-import com.maan.eway.common.res.GetAllMotorDetailsRes;
-import com.maan.eway.common.res.SearchPaymentInfoRes;
-import com.maan.eway.common.res.UpdateLapsedQuoteRes;
-
-import com.maan.eway.common.res.PortfolioCustomerDetailsRes;
 import com.maan.eway.common.res.SearchCustomerDetailsRes;
+import com.maan.eway.common.res.SearchPaymentInfoRes;
 import com.maan.eway.common.res.SearchPremiumDetailsRes;
 import com.maan.eway.common.res.SearchROPDetailsRes;
 import com.maan.eway.common.res.SearchROPVehicleDetailsRes;
 import com.maan.eway.common.res.SearchRes;
 import com.maan.eway.master.req.CopyQuoteDropDownReq;
-import com.maan.eway.res.CopyQuoteSuccessRes;
 import com.maan.eway.res.DropDownRes;
-import com.maan.eway.res.SuccessRes;
 
 public interface SearchService {
 
