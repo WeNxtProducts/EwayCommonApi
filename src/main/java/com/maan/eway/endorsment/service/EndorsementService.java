445c4ddada9a0436b3465341276792e22f98104c
Suganya.K <89253083+MaanRsa001@users.noreply.github.com>
2023-04-24 19:08:19 +0530

    Update EndorsementService.java

--- src/main/java/com/maan/eway/endorsment/service/EndorsementService.java
+++ src/main/java/com/maan/eway/endorsment/service/EndorsementService.java
@@ -303,7 +303,7 @@ public class EndorsementService {
 				Predicate n3 = cb.equal(m.get("productId"), request.getProductId());
 				Predicate n4 = cb.in(m.get("status")).value(Arrays.asList("E","P"));  // m.get("status").in("E","P"));
 				Predicate n5 = cb.or(cb.like(m.get("originalPolicyNo"), request.getPolicyNo()),cb.like(m.get("policyNo"), request.getPolicyNo()));
-			//	Predicate n6 = cb.equal(m.get("riskId"), "1");
+				//Predicate n6 = cb.equal(m.get("riskId"), "1");
 				//Predicate n7 = cb.like(h.get("quoteNo"), m.get("quoteNo"));
 				//Predicate n8 = cb.like(m.get("PolicyNo"), request.getPolicyNo());
 				//Predicate n5 = cb.lessThanOrEqualTo(m.get("updatedDate"), endDate);
@@ -330,7 +330,7 @@ public class EndorsementService {
 								m.get("productId"), m.get("branchCode"), m.get("requestReferenceNo"), m.get("quoteNo"),
 								m.get("customerId"), m.get("policyStartDate"), m.get("policyEndDate"))*/
 				
-				.groupBy(m.get("overallPremiumLc"),m.get("overallPremiumFc"))
+				.groupBy(m.get("overallPremiumLc"),m.get("overallPremiumFc"),m.get("policyNo"))
 						.orderBy(orderList);
 
 				// Get Result
