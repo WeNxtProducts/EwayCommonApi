4a8a41388b91b59bf2d9b56ce3ad5c823154e18c
Nandhini <maanrsa001@gmail.com>
2023-04-27 13:21:55 +0530

    policy req

--- src/main/java/com/maan/eway/master/req/PolicyTypeMasterGetReq.java
+++ src/main/java/com/maan/eway/master/req/PolicyTypeMasterGetReq.java
@@ -16,11 +16,11 @@ public class PolicyTypeMasterGetReq implements Serializable {
     @JsonProperty("PolicyTypeId")
     private String policyTypeId;
     
-//    @JsonProperty("InsuranceId")
-//    private String companyId;
-//
-//    @JsonProperty("ProductId")
-//    private String productId;
+    @JsonProperty("InsuranceId")
+    private String companyId;
+
+   @JsonProperty("ProductId")
+   private String productId;
 
     
 
