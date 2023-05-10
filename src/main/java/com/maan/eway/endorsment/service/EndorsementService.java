89df16c021df9d2efe21f051b40e88dd8f282012
Common 42 System <maansarovarwork@gmail.com>
2023-05-10 19:31:46 +0530

    endt issue.

--- src/main/java/com/maan/eway/endorsment/service/EndorsementService.java
+++ src/main/java/com/maan/eway/endorsment/service/EndorsementService.java
@@ -379,7 +379,7 @@ public class EndorsementService {
 				Predicate n1 = cb.equal(c.get("customerReferenceNo"), m.get("customerReferenceNo"));
 				Predicate n2 = cb.equal(m.get("companyId"), request.getCompanyId());
 				Predicate n3 = cb.equal(m.get("productId"), request.getProductId());
-				Predicate n4 = cb.in(m.get("status")).value(Arrays.asList("E","P","D"));  // m.get("status").in("E","P"));
+		//	Predicate n4 = cb.in(m.get("status")).value(Arrays.asList("E","P","D"));  // m.get("status").in("E","P"));
 				Predicate n5 = cb.or(cb.like(m.get("originalPolicyNo"), request.getPolicyNo()),cb.like(m.get("policyNo"), request.getPolicyNo()));
 				//Predicate n6 = cb.equal(m.get("riskId"), "1");
 				//Predicate n7 = cb.like(h.get("quoteNo"), m.get("quoteNo"));
@@ -403,7 +403,7 @@ public class EndorsementService {
 					n8 = e0.in(branches);
 				}*/
 
-				query.where(n1, n2, n3, n4, n5)
+				query.where(n1, n2, n3,/* n4,*/ n5)
 						/*.groupBy(c.get("customerReferenceNo"), c.get("idNumber"), c.get("clientName"), m.get("companyId"),
 								m.get("productId"), m.get("branchCode"), m.get("requestReferenceNo"), m.get("quoteNo"),
 								m.get("customerId"), m.get("policyStartDate"), m.get("policyEndDate"))*/
