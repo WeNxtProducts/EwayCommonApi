d0995312dce43e2d7902cf90c5d61b03bf040fcf
Samilla <89253083+MaanRsa001@users.noreply.github.com>
2023-05-23 10:57:07 +0530

    Update LoginValidationServiceImpl.java

--- src/main/java/com/maan/eway/admin/service/impl/LoginValidationServiceImpl.java
+++ src/main/java/com/maan/eway/admin/service/impl/LoginValidationServiceImpl.java
@@ -117,9 +117,11 @@ public class LoginValidationServiceImpl implements LoginValidationService  {
 				errors.add(new Error("06", "Attached Branch", "Please Choose Atleast One Branch"));
 			} 
 			
-			if( loginReq.getProductIds()==null || loginReq.getProductIds().size() == 0 ) {
-				errors.add(new Error("06", "ProductIds", "Please Choose Atleast One Product"));
-			} 
+			if(StringUtils.isNotBlank(loginReq.getSubUserType()) && ( loginReq.getSubUserType().equalsIgnoreCase("SuperAdmin")) ) { 
+				if( loginReq.getProductIds()==null || loginReq.getProductIds().size() == 0 ) {
+					errors.add(new Error("06", "ProductIds", "Please Choose Atleast One Product"));
+				} 
+			}
 			
 			// Additional Errors
 			if(StringUtils.isBlank(personalReq.getAddress1())  ) {
