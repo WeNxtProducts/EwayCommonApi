971f457a0a63c9a70cf089ede2f889f8f0f525f2
Maanrsa001 <89253083+MaanRsa001@users.noreply.github.com>
2023-06-22 19:39:33 +0530

    sectionid, locationid added

--- src/main/java/com/maan/eway/bean/ProductEmployeeDetails.java
+++ src/main/java/com/maan/eway/bean/ProductEmployeeDetails.java
@@ -55,6 +55,17 @@ private static final long serialVersionUID = 1L;
     @Id
     @Column(name="QUOTE_NO", nullable=false, length=20)
     private String     quoteNo ;
+    
+    @Id
+    @Column(name="SECTION_ID", nullable=false)
+    private String    sectionId ;
+    
+    @Id
+    @Column(name="LOCATION_ID", nullable=false)
+    private Integer     locationId ;
+    
+    @Column(name="SECTION_DESC")
+    private String     sectionDesc ;
 
     @Id
     @Column(name="RISK_ID", nullable=false)
