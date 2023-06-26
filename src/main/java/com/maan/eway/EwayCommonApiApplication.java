468042e54d34e3b7ff98e2a75896b93ec42fd84b
MaanRsa001 <maanrsa001@gmail.com>
2023-06-26 17:38:40 +0530

    Job runr enabled.

--- src/main/java/com/maan/eway/EwayCommonApiApplication.java
+++ src/main/java/com/maan/eway/EwayCommonApiApplication.java
@@ -1,19 +1,19 @@
 package com.maan.eway;
 
+import java.util.concurrent.Executor;
+
 import javax.sql.DataSource;
 
 import org.jobrunr.configuration.JobRunr;
 import org.jobrunr.scheduling.JobScheduler;
-import org.jobrunr.server.BackgroundJobServerConfiguration;
 import org.jobrunr.server.JobActivator;
-import org.jobrunr.server.configuration.BackgroundJobServerWorkerPolicy;
-import org.jobrunr.server.configuration.FixedSizeBackgroundJobServerWorkerPolicy;
 import org.jobrunr.storage.sql.common.SqlStorageProviderFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 @EnableCaching
 @SpringBootApplication
 @EnableAsync
@@ -23,7 +23,7 @@ public class EwayCommonApiApplication {
 		SpringApplication.run(EwayCommonApiApplication.class, args);
 	}
 	
-	  	/*@Bean(name = "NoticationThread-M")
+	  	@Bean(name = "NoticationThread-M")
 	    public Executor threadPoolTaskExecutor() {
 	  		ThreadPoolTaskExecutor t = new ThreadPoolTaskExecutor();
 	  		t.setCorePoolSize(2);
@@ -32,26 +32,8 @@ public class EwayCommonApiApplication {
 	  		t.setThreadNamePrefix("Mail(Async)-");
 	  		t.initialize();	  		
 	        return t;
-	    }*/
-	  	
-//	    @Bean
-//	    public JobScheduler initJobRunr(DataSource dataSource, JobActivator jobActivator) {
-//	       /* return JobRunr.configure()
-//	                .useJobActivator(jobActivator)
-//	                .useStorageProvider(SqlStorageProviderFactory
-//	                          .using(dataSource))
-//	                .useBackgroundJobServer()
-//	                .useDashboard(9879)	                
-//	                .initialize();*/
-//	    	return JobRunr.configure()
-//	    			.useJobActivator(jobActivator)
-//	    			.useStorageProvider(SqlStorageProviderFactory
-//	    			.using(dataSource))
-//	    			.useBackgroundJobServer()
-//	    			.useDashboard(9879)
-//	    			.initialize();
-//	    }
-
+	    }
+	
 	    @Bean
 	    public JobScheduler initJobRunr(DataSource dataSource, JobActivator jobActivator) {
 	        return JobRunr.configure()
