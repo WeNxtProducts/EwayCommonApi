fdcecdc138cb9a8bd9cd3960d9cb08b092f5d308
Maanrsa001 <89253083+MaanRsa001@users.noreply.github.com>
2023-07-04 11:57:51 +0530

    change password

--- src/main/java/com/maan/eway/auth/service/impl/LoginValidatedServiceImpl.java
+++ src/main/java/com/maan/eway/auth/service/impl/LoginValidatedServiceImpl.java
@@ -457,8 +457,8 @@ public class LoginValidatedServiceImpl implements LoginValidatedService {
 					} else if (model.getPassword().equalsIgnoreCase(epass)) {
 						list.add(new Error("", "ChangePassword", "Old Password  and New Password should not match"));
 					}
-				
-				else if(model.getLpass1().equals(epass) || model.getLpass2().equals(epass) || model.getLpass3().equals(epass) || model.getLpass4().equals(epass) || model.getLpass5().equals(epass)) {
+					
+				else if((model.getLpass1()!=null && model.getLpass1().equals(epass)) || (model.getLpass2()!=null&&model.getLpass2().equals(epass)) || (model.getLpass3()!=null&&model.getLpass3().equals(epass)) || (model.getLpass4()!=null&&model.getLpass4().equals(epass)) || (model.getLpass5()!=null&&model.getLpass5().equals(epass))) {
 					list.add(new Error("", "ChangePassword", "Newpassword should not be last 5 Password"));
 				} 
 			} 
