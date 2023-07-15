3fc248b885b60a16ba06262479fb29dc9e55f682
Samilla <89253083+MaanRsa001@users.noreply.github.com>
2023-07-15 12:54:29 +0530

    Update OccupationMasterServiceImpl.java

--- src/main/java/com/maan/eway/master/service/impl/OccupationMasterServiceImpl.java
+++ src/main/java/com/maan/eway/master/service/impl/OccupationMasterServiceImpl.java
@@ -221,13 +221,12 @@ try {
 	Predicate n6 = cb.equal(c.get("branchCode"),"99999");
 	Predicate n7 = cb.or(n5,n6);
 	Predicate n8 = cb.equal(c.get("productId"),req.getProductId());
-	if(StringUtils.isBlank(req.getProductId())) {
-		query.where(n12,n2,n3,n4,n7).orderBy(orderList);
+	Predicate n9 = cb.equal(c.get("productId"),"99999");
+	Predicate n10 = cb.or(n8,n9);
+	
+	query.where(n12,n2,n3,n4,n7,n10).orderBy(orderList);
 		
-	}
-	else {
-	query.where(n12,n2,n3,n4,n7,n8).orderBy(orderList);
-	}
+	
 	TypedQuery<OccupationMaster> result = em.createQuery(query);
 	list = result.getResultList();
 	list = list.stream().filter(distinctByKey(o -> Arrays.asList(o.getOccupationId()))).collect(Collectors.toList());
