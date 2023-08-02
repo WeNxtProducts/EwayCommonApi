225c3bddfcc66505d829212480291fa2b869049c
Common 42 System <maansarovarwork@gmail.com>
2023-08-02 10:54:45 +0530

    Update FactorRateRequestDetailsServiceImpl.java

--- src/main/java/com/maan/eway/service/impl/FactorRateRequestDetailsServiceImpl.java
+++ src/main/java/com/maan/eway/service/impl/FactorRateRequestDetailsServiceImpl.java
@@ -2058,7 +2058,7 @@ this.repository = repo;
 				resp=calcEngine.referalCalculator(engine);
 			}else {
 				EndtTypeMaster endt=ratingutil.getEndtMasterData(engine.getInsuranceId(), engine.getProductId(),endtTypdId);
-				engine.setCoverModification(endt.getIsCoverendt());)
+				engine.setCoverModification(endt.getIsCoverendt());
 				resp=calcEngine.endorsementCalculator(engine,endtCount);
 			}
 			 
