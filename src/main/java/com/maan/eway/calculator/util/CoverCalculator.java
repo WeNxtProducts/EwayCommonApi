d8182b60bb9c3877bb6622b36a46676e65d686be
MaanRsa001 <maanrsa001@gmail.com>
2023-08-28 19:42:17 +0530

    calc

--- src/main/java/com/maan/eway/calculator/util/CoverCalculator.java
+++ src/main/java/com/maan/eway/calculator/util/CoverCalculator.java
@@ -51,7 +51,10 @@ public class CoverCalculator extends CommonCalculator implements Consumer<Cover>
 				 
 				 
 				 
-				 BigDecimal si=vehicles.get(0).get(t.getCoverBasedOn())==null?BigDecimal.ZERO:new BigDecimal(vehicles.get(0).get(t.getCoverBasedOn()).toString());
+				 BigDecimal si=BigDecimal.ZERO;
+				 if(!"A".equals(t.getCalcType()))
+					 si=vehicles.get(0).get(t.getCoverBasedOn())==null?BigDecimal.ZERO:new BigDecimal(vehicles.get(0).get(t.getCoverBasedOn()).toString());
+				 
 				 if("Y".equals(t.getDependentCoveryn())) {
 					 if(calculatedcover!=null) {
 						Cover ct = calculatedcover.stream().filter(c->c.getCoverId().equals(t.getDependentCoverId())).findAny().orElse(null);
