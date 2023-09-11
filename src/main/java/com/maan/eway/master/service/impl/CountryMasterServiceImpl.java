23f8ca46d7a0952d2753aa41f864650bd188a950
Maanrsa001 <89253083+MaanRsa001@users.noreply.github.com>
2023-09-11 10:42:55 +0530

    country

--- src/main/java/com/maan/eway/master/service/impl/CountryMasterServiceImpl.java
+++ src/main/java/com/maan/eway/master/service/impl/CountryMasterServiceImpl.java
@@ -747,16 +747,13 @@ public class CountryMasterServiceImpl implements CountryMasterService {
 			Predicate n12 = cb.or(n1,n11);
 			javax.persistence.criteria.Predicate n2 = cb.equal(c.get("effectiveDateStart"), effectiveDate);
 			javax.persistence.criteria.Predicate n3 = cb.equal(c.get("effectiveDateEnd"), effectiveDate2);
-//			Predicate n4 = null;
-//			if(StringUtils.isBlank(req.getInsuranceId()))
-//				n4 = cb.equal(c.get("companyId"),  "99999" );
-//			else
-//				n4 = cb.equal(c.get("companyId"), req.getInsuranceId() );
-			Predicate n4 = cb.equal(c.get("companyId"), req.getInsuranceId() );
-			Predicate n5 = cb.equal(c.get("companyId"), "99999"  );
-			Predicate n6 = cb.or(n4,n5);
+			Predicate n4 = null;
+			if(StringUtils.isBlank(req.getInsuranceId()))
+				n4 = cb.equal(c.get("companyId"),  "99999" );
+			else
+				n4 = cb.equal(c.get("companyId"), req.getInsuranceId() );
 		
-			query.where(n12, n2,n3,n6).orderBy(orderList);
+			query.where(n12, n2,n3,n4).orderBy(orderList);
 
 			// Get Result
 			TypedQuery<CountryMaster> result = em.createQuery(query);
