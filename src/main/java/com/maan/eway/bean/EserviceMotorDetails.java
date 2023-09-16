8b193a176873372b13f0e87c73f66b614a83ce83
Maanrsa001 <89253083+MaanRsa001@users.noreply.github.com>
2023-09-16 18:01:23 +0530

    entity

--- src/main/java/com/maan/eway/bean/EserviceMotorDetails.java
+++ src/main/java/com/maan/eway/bean/EserviceMotorDetails.java
@@ -546,8 +546,8 @@ private static final long serialVersionUID = 1L;
     private String     customerName ;
     
 
-    @Column(name="BRANCH_CODE")
-    private Integer     branchName ;
+    @Column(name="BRANCH_NAME")
+    private String     branchName ;
 }
 
 
