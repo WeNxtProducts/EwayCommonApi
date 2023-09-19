7a1b217314daa9a34fb3b0170e900b88441518da
Samilla <89253083+MaanRsa001@users.noreply.github.com>
2023-09-19 12:39:33 +0530

    Update QuoteThreadCall.java

--- src/main/java/com/maan/eway/common/service/impl/QuoteThreadCall.java
+++ src/main/java/com/maan/eway/common/service/impl/QuoteThreadCall.java
@@ -3003,7 +3003,7 @@ public class QuoteThreadCall implements Callable<Object>  {
 		    home.setBrokerCode(motorData.getBrokerCode());
 		    home.setAgencyCode(Integer.valueOf(motorData.getAgencyCode().replaceAll("\\r", "" ).replaceAll("\\n", "" ) )) ;
 		    home.setCustomerCode(motorData.getCustomerCode());
-		    home.setCustomerName( motorData.getCustomerName() );
+		    home.setCustomerName(motorData.getBdmCode() !=null ? motorData.getCustomerName() :custData.getClientName());
 		    home.setBdmCode(motorData.getBdmCode());
 			home.setBrokerBranchCode(motorData.getBrokerBranchCode() );
 			home.setBrokerBranchName(motorData.getBrokerBranchName() );
@@ -3118,7 +3118,7 @@ public class QuoteThreadCall implements Callable<Object>  {
 		    home.setBrokerCode(travelData.getBrokerCode());
 		    home.setAgencyCode(Integer.valueOf(travelData.getAgencyCode().replaceAll("\\r", "" ).replaceAll("\\n", "" ) ));
 		    home.setCustomerCode(travelData.getCustomerCode());
-		    home.setCustomerName( travelData.getCustomerName() );
+		    home.setCustomerName(travelData.getBdmCode() !=null ? travelData.getCustomerName() :custData.getClientName());
 		    home.setBdmCode(travelData.getBdmCode());
 			home.setBrokerBranchCode(travelData.getBrokerBranchCode() );
 			home.setBrokerBranchName(travelData.getBrokerBranchName() );
@@ -3198,7 +3198,7 @@ public class QuoteThreadCall implements Callable<Object>  {
 		    home.setBrokerCode(buildingData.getBrokerCode());
 		    home.setAgencyCode(Integer.valueOf(buildingData.getAgencyCode().replaceAll("\\r", "" ).replaceAll("\\n", "" ) ));
 		    home.setCustomerCode(buildingData.getCustomerCode());
-		    home.setCustomerName(buildingData.getCustomerName() );
+		    home.setCustomerName(buildingData.getBdmCode() !=null ? buildingData.getCustomerName() :custData.getClientName());
 		    home.setBdmCode(buildingData.getBdmCode());
 			home.setBrokerBranchCode(buildingData.getBrokerBranchCode() );
 			home.setBrokerBranchName(buildingData.getBrokerBranchName() );
