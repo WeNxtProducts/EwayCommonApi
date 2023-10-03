e7cfef87f5a2fad9a68a3cf2058ea64280ae7b2f
Samilla <89253083+MaanRsa001@users.noreply.github.com>
2023-10-03 17:39:45 +0530

    Is COver Endt

--- src/main/java/com/maan/eway/endorsment/request/EndorsementType.java
+++ src/main/java/com/maan/eway/endorsment/request/EndorsementType.java
@@ -33,4 +33,8 @@ public class EndorsementType {
 	private String sectionModificationYn;
 	@JsonProperty("SectionModificationType") 
 	private String sectionModificationType;
+	@JsonProperty("isCoverEndt") 
+	private String isCoverEndt;
+	
+	
 }
