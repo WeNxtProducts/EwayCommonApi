05819d23ba929ed3b301cc5927d6e3fdcdde44a6
Bhuvanesh <maanrsa001@gmail.com>
2023-10-12 11:38:45 +0530

    issues Fixed

--- src/main/java/com/maan/eway/jasper/service/impl/JasperCustomServiceImple.java
+++ src/main/java/com/maan/eway/jasper/service/impl/JasperCustomServiceImple.java
@@ -204,7 +204,8 @@ public class JasperCustomServiceImple {
 				piRoot.get("vrTinNo").alias("vrTinNo"),cb.selectCase().when(cb.equal(piRoot.get("idType"), "6"), piRoot.get("idNumber")).alias("customerTin"),
 				hpmRoot.get("policyNo").alias("policyNo"),hpmRoot.get("quoteNo").alias("quoteNo"),hpmRoot.get("inceptionDate").alias("inceptionDate"),
 				hpmRoot.get("expiryDate").alias("expiryDate"),hpmRoot.get("currency").alias("currency"),hpmRoot.get("debitNoteNo").alias("debitNoteNo"),
-				vrnNumber.alias("vrnNumber"),tinNumber.alias("tinNumber"),brokerName.alias("brokerName"),
+				vrnNumber.alias("vrnNumber"),tinNumber.alias("tinNumber"),cb.selectCase().when(cb.in(hpmRoot.get("sourceType")).value(Arrays.asList("Premia Broker","Premia Direct","Premia Agent")),hpmRoot.get("customerName"))
+					.otherwise(brokerName).alias("brokerName"),
 				cb.selectCase().when(cb.in(hpmRoot.get("currency")).value(currencyId), hpmRoot.get("premiumLc")).otherwise(hpmRoot.get("premiumFc")).alias("premium"),
 				cb.selectCase().when(cb.in(hpmRoot.get("currency")).value(currencyId), hpmRoot.get("vatPremiumLc")).otherwise(hpmRoot.get("vatPremiumFc")).alias("vatPremium"),
 				cb.selectCase().when(cb.in(hpmRoot.get("currency")).value(currencyId), hpmRoot.get("overallPremiumLc")).otherwise(hpmRoot.get("overallPremiumFc")).alias("overAllPremium"),
