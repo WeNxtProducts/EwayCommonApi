192f02a7d1e74aa78052ad8264cad609cb83a033
Samilla <89253083+MaanRsa001@users.noreply.github.com>
2023-11-07 18:26:10 +0530

    Update LoginDetailsServiceImpl.java

--- src/main/java/com/maan/eway/admin/service/impl/LoginDetailsServiceImpl.java
+++ src/main/java/com/maan/eway/admin/service/impl/LoginDetailsServiceImpl.java
@@ -994,13 +994,15 @@ this.repository = repo;
 			// Where
 			Predicate n1 = cb.equal(l.get("loginId"), u.get("loginId"));
 			Predicate n2 = cb.equal(l.get("userType"), req.getUserType());
-			Predicate n4 = cb.like(l.get("companyId"), "%"+req.getCompanyId()+"%");
+			Predicate n4 = cb.equal(l.get("companyId"), req.getCompanyId());
+			Predicate n5 = cb.like(l.get("attachedCompanies"), "%"+req.getCompanyId()+"%");
+			Predicate n6 = cb.or(n4,n5);
 			if(StringUtils.isNotBlank( req.getSubUserType())  ) {
 				Predicate n3 = cb.equal(l.get("subUserType"), req.getSubUserType());
-				query.where(n1,n2,n3,n4).orderBy(orderList);
+				query.where(n1,n2,n3,n6).orderBy(orderList);
 				
 			} else {
-				query.where(n1,n2,n4).orderBy(orderList);
+				query.where(n1,n2,n6).orderBy(orderList);
 			}
 			
 			// Get Result
