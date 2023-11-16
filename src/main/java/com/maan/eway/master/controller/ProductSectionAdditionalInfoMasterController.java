47f0bd5c5404aadd13afce5f94f2cf7ca91c1504
Maanrsa001 <89253083+MaanRsa001@users.noreply.github.com>
2023-11-16 15:20:36 +0530

    additional info

--- src/main/java/com/maan/eway/master/controller/ProductSectionAdditionalInfoMasterController.java
+++ src/main/java/com/maan/eway/master/controller/ProductSectionAdditionalInfoMasterController.java
@@ -1,8 +1,11 @@
 package com.maan.eway.master.controller;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -10,15 +13,29 @@ import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.maan.eway.common.res.CommonRes;
+import com.maan.eway.document.controller.DocumentController;
+import com.maan.eway.error.CommonValidationException;
+import com.maan.eway.error.Error;
+import com.maan.eway.master.req.FileUploadReq;
 import com.maan.eway.master.req.GetOptedSectionAdditionalInfoReq;
+import com.maan.eway.master.req.GetSectionAdditionalDetailsReq;
+import com.maan.eway.master.req.InsertAdditionalInfoReq;
 import com.maan.eway.master.res.GetOptedSectionAdditionalInfoRes;
+import com.maan.eway.master.res.GetSectionAdditionalDetailsRes;
 import com.maan.eway.master.service.ProductSectionAdditionalInfoMasterService;
+import com.maan.eway.res.SuccessRes;
 import com.maan.eway.service.PrintReqService;
 
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 
 @RestController
 @Api(tags = "MASTER : Product Section Additional Info Master", description = "API's")
@@ -33,7 +50,7 @@ public class ProductSectionAdditionalInfoMasterController {
 	
 	
 	@PreAuthorize("hasAnyRole('ROLE_APPROVER','ROLE_USER')")
-	@PostMapping("/getoptedsectionadditionalinfo") 	//based on product and opted sections
+	@PostMapping("/getoptedsectionadditionalinfo") 	//based on product and opted sections (Quote Flow)
 	public ResponseEntity<CommonRes> getOptedSectionAdditionalInfo(@RequestBody GetOptedSectionAdditionalInfoReq req){
 		CommonRes data = new CommonRes();
 			reqPrinter.reqPrint(req);
@@ -50,5 +67,93 @@ public class ProductSectionAdditionalInfoMasterController {
 		}
 	}
 	
+	@PreAuthorize("hasAnyRole('ROLE_APPROVER','ROLE_USER','ROLE_ADMIN')")
+	@PostMapping("/insertadditionalinfo")
+	@ApiOperation(value = "This method is Insert Additional Info")
+	public ResponseEntity<CommonRes> insertAdditionalInfo(@RequestBody InsertAdditionalInfoReq req) {
 
+		reqPrinter.reqPrint(req);
+		CommonRes data = new CommonRes();
+
+		List<Error> validation = service.validateAdditionalInfo(req);
+		// validation
+		if (validation != null && validation.size() != 0) {
+			data.setCommonResponse(null);
+			data.setIsError(true);
+			data.setErrorMessage(validation);
+			data.setMessage("Failed");
+			return new ResponseEntity<CommonRes>(data, HttpStatus.OK);
+
+		} else {
+
+			// Get All
+			SuccessRes res = service.insertAdditionalInfo(req);
+			data.setCommonResponse(res);
+			data.setIsError(false);
+			data.setErrorMessage(Collections.emptyList());
+			data.setMessage("Success");
+
+			if (res != null) {
+				return new ResponseEntity<CommonRes>(data, HttpStatus.CREATED);
+			} else {
+				return new ResponseEntity<>(null, HttpStatus.BAD_REQUEST);
+			}
+		}
+
+	}
+	
+	//  Get By Section Id (Admin side)
+		@PreAuthorize("hasAnyRole('ROLE_ADMIN','ROLE_APPROVER')")
+		@PostMapping("/getsectionadditionaldetails")
+		@ApiOperation("This method is get section additional details")
+		public ResponseEntity<CommonRes> getSectionAdditionalDetails(@RequestBody GetSectionAdditionalDetailsReq req)
+		{
+			CommonRes data = new CommonRes();
+			reqPrinter.reqPrint(req);
+			
+			GetSectionAdditionalDetailsRes res = service.getSectionAdditionalDetails(req);
+			data.setCommonResponse(res);
+			data.setErrorMessage(Collections.emptyList());
+			data.setIsError(false);
+			data.setMessage("Success");
+			
+			if(res!= null) {
+				return new ResponseEntity<CommonRes> (data, HttpStatus.CREATED);
+			}
+			else {
+				return new ResponseEntity<> (null, HttpStatus.BAD_REQUEST);
+			}
+		}
+
+		private Logger log = LogManager.getLogger(DocumentController.class);
+		
+		//File Upload
+		@PreAuthorize("hasAnyRole('ROLE_ADMIN','ROLE_APPROVER','ROLE_USER')")
+		@PostMapping("/upload")
+		@ApiOperation(value = "This method is to Upload Document")
+		public ResponseEntity<CommonRes> uploadFile(@RequestParam("File") MultipartFile file) throws CommonValidationException, JsonMappingException, JsonProcessingException{
+			
+//			log.info(jsonString);
+//			
+//			FileUploadReq req =  new ObjectMapper().readValue(jsonString, FileUploadReq.class); 
+			
+	    	List<Error> error = new ArrayList<Error>();
+			error = service.docvalidation(file);
+			if (error != null && error.size() > 0) {
+				
+				CommonRes res = new CommonRes();
+				res.setCommonResponse(null);
+				res.setIsError(true);
+				res.setErrorMessage(error);
+				res.setMessage("Success");
+				
+				return ResponseEntity.status(HttpStatus.EXPECTATION_FAILED).body(res);
+				
+			}else {
+				CommonRes res = service.fileupload(file);
+				return ResponseEntity.status(HttpStatus.OK).body(res);
+			}
+			
+		}
+		
 }
---
+++ src/main/java/com/maan/eway/repository/ProductSectionAdditionalInfoMasterRepo.java
@@ -0,0 +1,15 @@
+package com.maan.eway.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.maan.eway.bean.ProductSectionAdditionalInfoMaster;
+import com.maan.eway.bean.ProductSectionAdditionalInfoMasterId;
+
+public interface ProductSectionAdditionalInfoMasterRepo extends JpaRepository< ProductSectionAdditionalInfoMaster,  ProductSectionAdditionalInfoMasterId>{
+
+	List<ProductSectionAdditionalInfoMaster> findByProductIdAndSectionIdAndCompanyId(Integer valueOf, Integer valueOf2,
+			String companyId);
+
+}
