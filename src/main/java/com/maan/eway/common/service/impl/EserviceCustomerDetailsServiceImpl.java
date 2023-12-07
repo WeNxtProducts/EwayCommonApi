1836aa7d40cbb0e89e005dd3f332b8a20910a7fa
MaanRsa001 <maanrsa001@gmail.com>
2023-12-07 18:33:01 +0530

    test

--- src/main/java/com/maan/eway/common/service/impl/EserviceCustomerDetailsServiceImpl.java
+++ src/main/java/com/maan/eway/common/service/impl/EserviceCustomerDetailsServiceImpl.java
@@ -524,9 +524,9 @@ public class EserviceCustomerDetailsServiceImpl implements EserviceCustomerDetai
 		
 						}
 
-					}else {
+					}/*else {
 						errorList.add(new Error("38", "DobOrRegDate", "DobOrRegDate Not Valid"));
-					}
+					}*/
 					}catch (Exception e) {
 						errorList.add(new Error("38", "DobOrRegDate", "DobOrRegDate Not Valid"));
 					}
@@ -555,9 +555,9 @@ public class EserviceCustomerDetailsServiceImpl implements EserviceCustomerDetai
 							}
 						}
 
-					}else {
+					}/*else {
 						errorList.add(new Error("38", "DobOrRegDate", "DobOrRegDate Not Valid"));
-					}
+					}*/
 					}catch (Exception e) {
 						errorList.add(new Error("38", "DobOrRegDate", "DobOrRegDate Not Valid"));
 					}
