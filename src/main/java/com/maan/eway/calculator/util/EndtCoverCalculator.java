0bc9809c9878df34382b167f0ae57097dfc22629
MaanRsa001 <maanrsa001@gmail.com>
2023-12-15 12:28:06 +0530

    endt issue

--- src/main/java/com/maan/eway/calculator/util/EndtCoverCalculator.java
+++ src/main/java/com/maan/eway/calculator/util/EndtCoverCalculator.java
@@ -163,9 +163,9 @@ public class EndtCoverCalculator  extends CommonCalculator implements Consumer<C
 					//new premium-old prem
 					endorsement = t.getEndorsements().get(0);
 					boolean dontGo=false;
+					endorsement.setMinimumPremiumYn(t.getMinimumPremiumYn());
 
-
-
+					
 					if("Y".equals(engine.getCoverModification())) {
 
 						if(("Y".equals(t.getProRataYn()) || "D".equals(t.getProRataYn())) && "Y".equals(t.getUserOpt())) {
