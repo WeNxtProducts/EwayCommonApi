d0c5e299ff438eb8ba59d425177cecc8595d0185
MaanRsa001 <maanrsa001@gmail.com>
2023-12-18 13:18:54 +0530

    endt issue

--- src/main/java/com/maan/eway/calculator/util/EndtCoverCalculator.java
+++ src/main/java/com/maan/eway/calculator/util/EndtCoverCalculator.java
@@ -366,7 +366,7 @@ public class EndtCoverCalculator  extends CommonCalculator implements Consumer<C
 					 		 
 
 					 		 if("Y".equals(endorsement.getMinimumPremiumYn())) { // if previous endorsment is minimum Premium dont calculation only subtract ... 14/12/2023 lalit sir told 
-					 			domath = t.getPremiumExcluedTax().subtract(endorsement.getPremiumExcluedTax());
+					 			domath = t.getPremiumExcluedTax().subtract(endorsement.getMinimumPremium());  ///endorsement.getPremiumExcluedTax());
 					 		 }else if("Y".equals(t.getMinimumPremiumYn()) && !"A".equals(t.getCalcType())){
 					 			 BigDecimal actualSumInsured = t.getMinimumPremium().divide(new BigDecimal(endorsement.getEndorsementRate()/100),MathContext.DECIMAL32);
 					 			 BigDecimal derivedSumInsred = totalSumInsuredendt.subtract(actualSumInsured);
