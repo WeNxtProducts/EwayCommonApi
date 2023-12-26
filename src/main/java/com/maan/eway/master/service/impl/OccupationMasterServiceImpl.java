ef973ea5d9e278c729807f6a6aade3ea456f327b
suganya <89253083+MaanRsa001@users.noreply.github.com>
2023-12-26 15:43:39 +0530

    Update OccupationMasterServiceImpl.java

--- src/main/java/com/maan/eway/master/service/impl/OccupationMasterServiceImpl.java
+++ src/main/java/com/maan/eway/master/service/impl/OccupationMasterServiceImpl.java
@@ -220,17 +220,21 @@ try {
 	Predicate n5 = cb.equal(c.get("branchCode"),req.getBranchCode());
 	Predicate n6 = cb.equal(c.get("branchCode"),"99999");
 	Predicate n7 = cb.or(n5,n6);
-	Predicate n8 = null ;
-	if(StringUtils.isNotBlank(req.getProductId() )) {
-		n8 = cb.equal(c.get("productId"),req.getProductId());
-	} else {
-		n8 = cb.equal(c.get("productId"),"99999");
-	}
+	
+	Predicate n8= cb.equal(c.get("productId"),req.getProductId());
+	Predicate n9= cb.equal(c.get("productId"),"99999");
+	Predicate n10 = cb.or(n8,n9);
+	
+//	if(StringUtils.isNotBlank(req.getProductId() )) {
+//		n8 = cb.equal(c.get("productId"),req.getProductId());
+//	} else {
+//		n8 = cb.equal(c.get("productId"),"99999");
+//	}
 	
 	//Predicate n9 = cb.equal(c.get("productId"),"99999");
 //	Predicate n10 = cb.or(n8,);
 	
-	query.where(n12,n2,n3,n4,n7,n8).orderBy(orderList);
+	query.where(n12,n2,n3,n4,n7,n10).orderBy(orderList);
 		
 	
 	TypedQuery<OccupationMaster> result = em.createQuery(query);
