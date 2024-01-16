c69f0a04a3e4d769f5d343025c6d352f6191a371
Common 42 System <89253083+MaanRsa001@users.noreply.github.com>
2024-01-16 16:59:57 +0530

    issue fixeed

--- src/main/java/com/maan/eway/admin/res/LoginBrokerDetailsGridRes.java
+++ src/main/java/com/maan/eway/admin/res/LoginBrokerDetailsGridRes.java
@@ -88,4 +88,6 @@ public class LoginBrokerDetailsGridRes {
 	
 	@JsonProperty("CustomerName")
     private String    customerName;
+	@JsonProperty("RegulatoryCode")
+	private String regulatoryCode;
 }
