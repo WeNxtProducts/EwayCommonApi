1044ed562cd9284837f5fee33f113f534699d449
Samilla <89253083+MaanRsa001@users.noreply.github.com>
2024-01-25 17:27:59 +0530

    Kenya keys

--- src/main/java/com/maan/eway/bean/EserviceMotorDetails.java
+++ src/main/java/com/maan/eway/bean/EserviceMotorDetails.java
@@ -668,6 +668,11 @@ private static final long serialVersionUID = 1L;
      
      @Column(name="POWER_WATTS", length=20)
      private BigDecimal     powerWatts ;
+     
+
+     @Column(name="VEHICLE_GROUP", length=20)
+     private String     vehicleGroup ;
+     
 }
 
 
