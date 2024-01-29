f01b413ac6a16b67b1ad3703fcd670f06a194a6d
suganya <89253083+MaanRsa001@users.noreply.github.com>
2024-01-29 16:41:46 +0530

    enitity

--- src/main/java/com/maan/eway/bean/EserviceMotorDetails.java
+++ src/main/java/com/maan/eway/bean/EserviceMotorDetails.java
@@ -93,6 +93,7 @@ private static final long serialVersionUID = 1L;
     private String     policyNo;
 
     
+    
     @Column(name="MOTOR_CATEGORY", length=1)
     private String     motorCategory ;
 
@@ -672,6 +673,11 @@ private static final long serialVersionUID = 1L;
 
      @Column(name="VEHICLE_GROUP", length=20)
      private String     vehicleGroup ;
+     
+     @Column(name="CAR_ALARM_YN", length=20)
+     private String     carAlarmYn ;
+     
+     
      
 }
 
