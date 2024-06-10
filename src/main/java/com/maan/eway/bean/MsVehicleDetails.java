66883536bd6ea718304fa125103ec4f81d9190c8
MaanRsa001 <maanrsa001@gmail.com>
2024-06-10 16:05:20 +0530

    fix

--- src/main/java/com/maan/eway/bean/MsVehicleDetails.java
+++ src/main/java/com/maan/eway/bean/MsVehicleDetails.java
@@ -333,6 +333,10 @@ private static final long serialVersionUID = 1L;
 
 	@Column(name="LOSS_RATIO")
 	private Double lossRatio;
+	
+
+	@Column(name = "PREVIOUS_INSURANCE_YN")
+	private String previousInsuranceYN;
 }
 
 
