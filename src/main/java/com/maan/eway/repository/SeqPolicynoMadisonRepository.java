b5ad0497f9aeafed4fac80eddde7c0fa7c7728fe
Suganya <89253083+MaanRsa001@users.noreply.github.com>
2024-06-11 11:11:04 +0530

    Update SeqPolicynoMadisonRepository.java

--- src/main/java/com/maan/eway/repository/SeqPolicynoMadisonRepository.java
+++ src/main/java/com/maan/eway/repository/SeqPolicynoMadisonRepository.java
@@ -2,9 +2,11 @@ package com.maan.eway.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
-
+import java.math.BigDecimal;
 import com.maan.eway.bean.SeqPolicynoMadison;
 
 public interface SeqPolicynoMadisonRepository extends JpaRepository<SeqPolicynoMadison,Long > , JpaSpecificationExecutor<SeqPolicynoMadison> {
-
+  
+	
+	
 }
