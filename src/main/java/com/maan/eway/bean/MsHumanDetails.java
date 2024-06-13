fcfd103819ca63cad9ee14ac6743e3fb621e0a7b
sneha <89253083+MaanRsa001@users.noreply.github.com>
2024-06-13 16:34:26 +0530

    Entity Changes

--- src/main/java/com/maan/eway/bean/MsHumanDetails.java
+++ src/main/java/com/maan/eway/bean/MsHumanDetails.java
@@ -276,4 +276,10 @@ private static final long serialVersionUID = 1L;
    
    @Column(name="INDEMNITY_SUMINSURED")
    private Double  Indemnitysuminsured  ;
+   
+   @Column(name="DOMESTIC_SERVENT_SI")
+   private BigDecimal       domesticServentSi;
+   
+   @Column(name="DOMESTIC_SERVENT_SI_LC")
+   private BigDecimal       domesticServentSiLc;
 }
