587edd60c91fcdccaf0fe93f9067d96824c89293
MaanRsa001 <maanrsa001@gmail.com>
2024-08-31 16:46:18 +0530

    added service anotation

--- src/main/java/com/maan/eway/renewal/service/impl/RenewalServiceImpl.java
+++ src/main/java/com/maan/eway/renewal/service/impl/RenewalServiceImpl.java
@@ -1,9 +1,11 @@
 package com.maan.eway.renewal.service.impl;
 
+import org.springframework.stereotype.Service;
+
 import com.maan.eway.common.res.CommonRes;
 import com.maan.eway.renewal.req.pullrenewalReq;
 import com.maan.eway.renewal.service.RenewalService;
-
+@Service
 public class RenewalServiceImpl implements RenewalService{
 
 	@Override
