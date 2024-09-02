8bded5cd4b0d3959fb4fdb6b18d4a07a49b79a66
sneha <89253083+MaanRsa001@users.noreply.github.com>
2024-09-02 18:28:56 +0530

    ClaimType id fixed

--- src/main/java/com/maan/eway/bean/MsVehicleDetails.java
+++ src/main/java/com/maan/eway/bean/MsVehicleDetails.java
@@ -360,6 +360,11 @@ private static final long serialVersionUID = 1L;
 	@Column(name = "PA_COVERID")
     private String paCoverId;
 	
+	
+	@Column(name = "CLAIM_TYPE", length = 20)
+	private String claimType;
+
+	
 	@Column(name = "NO_OF_CYLINDERS")
     private String noOfCylinders;
 }
