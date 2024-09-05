2866ca6598688fb8ccceefc4022e382b29a1ac07
Suganya <89253083+MaanRsa001@users.noreply.github.com>
2024-09-05 12:03:21 +0530

    Buypolicy

--- src/main/java/com/maan/eway/common/service/impl/QuoteThreadCall.java
+++ src/main/java/com/maan/eway/common/service/impl/QuoteThreadCall.java
@@ -675,8 +675,8 @@ public class QuoteThreadCall implements Callable<Object>  {
 			}else {
 				//***EserviceCommonDetails commonData = eserCommonRepo.findByRequestReferenceNoAndRiskId(request.getRequestReferenceNo(),request.getVehicleIdsList().get(0).getVehicleId());
 //				EserviceCommonDetails commonData = eserCommonRepo.findByRequestReferenceNoAndRiskIdAndSectionId(request.getRequestReferenceNo(),request.getVehicleIdsList().get(0).getVehicleId(),sectionid);
-				EserviceCommonDetails commonData = eserCommonRepo.findByRequestReferenceNoAndOriginalRiskIdAndSectionId(request.getRequestReferenceNo(),request.getVehicleIdsList().get(0).getVehicleId(),sectionid);
-				
+//				EserviceCommonDetails commonData = eserCommonRepo.findByRequestReferenceNoAndOriginalRiskIdAndSectionId(request.getRequestReferenceNo(),request.getVehicleIdsList().get(0).getVehicleId(),sectionid);
+				EserviceCommonDetails commonData = eserCommonRepo.findByRequestReferenceNo(request.getRequestReferenceNo() ).get(0);
 				customerRefNo = commonData.getCustomerReferenceNo();
 				
 			}
@@ -3710,9 +3710,10 @@ public class QuoteThreadCall implements Callable<Object>  {
 		HomePositionMaster home = new HomePositionMaster();
 		try {
 			String sectionid=request.getVehicleIdsList().get(0).getSectionId();
-			//EserviceCommonDetails  eserCommonData = eserCommonRepo.findByRequestReferenceNoAndRiskId(request.getRequestReferenceNo() , request.getVehicleId()) ;
-			//***
-			EserviceCommonDetails  eserCommonData = eserCommonRepo.findByRequestReferenceNoAndOriginalRiskIdAndSectionId(request.getRequestReferenceNo() , request.getVehicleId(),sectionid) ;
+			Integer locationid=request.getVehicleIdsList().get(0).getLocationId();
+			EserviceCommonDetails  eserCommonData = eserCommonRepo.findByRequestReferenceNoAndRiskIdAndSectionIdAndLocationId(request.getRequestReferenceNo(), request.getVehicleId(), sectionid, locationid);
+		
+//			EserviceCommonDetails  eserCommonData = eserCommonRepo.findByRequestReferenceNoAndOriginalRiskIdAndSectionId(request.getRequestReferenceNo() , request.getVehicleId(),sectionid) ;
 			Long commonCount =  eserCommonRepo.countByRequestReferenceNo(request.getRequestReferenceNo() ) ;
 			EserviceCustomerDetails custData = eserCustRepo.findByCustomerReferenceNo(eserCommonData.getCustomerReferenceNo());
 			
