1be09ec10ef9c92cf39c31aa4e69aa9c659e28da
sneha <89253083+MaanRsa001@users.noreply.github.com>
2024-10-26 12:25:30 +0530

    ModelMake DropDown BodyType Remove

--- src/main/java/com/maan/eway/master/service/impl/MotorMakeModelMasterServiceImpl.java
+++ src/main/java/com/maan/eway/master/service/impl/MotorMakeModelMasterServiceImpl.java
@@ -891,10 +891,10 @@ public class MotorMakeModelMasterServiceImpl implements MotorMakeModelMasterServ
 			Predicate a1 = cb.equal(c.get("modelId"),ocpm1.get("modelId"));
 			Predicate a2 = cb.lessThanOrEqualTo(ocpm1.get("effectiveDateStart"), today);
 			Predicate a3 = cb.equal(c.get("makeId"),ocpm1.get("makeId"));
-			Predicate a11 = cb.equal(c.get("bodyId"),ocpm1.get("bodyId"));
+			//Predicate a11 = cb.equal(c.get("bodyId"),ocpm1.get("bodyId"));
 			Predicate a7 = cb.equal(c.get("companyId"),ocpm1.get("companyId"));
 			Predicate a8 = cb.equal(c.get("branchCode"),ocpm1.get("branchCode"));
-			effectiveDate.where(a1,a2,a3,a7,a8,a11);
+			effectiveDate.where(a1,a2,a3,a7,a8);
 			// Effective Date End Max Filter
 			Subquery<Timestamp> effectiveDate2 = query.subquery(Timestamp.class);
 			Root<MotorMakeModelMaster> ocpm2 = effectiveDate2.from(MotorMakeModelMaster.class);
@@ -904,8 +904,8 @@ public class MotorMakeModelMasterServiceImpl implements MotorMakeModelMasterServ
 			Predicate a6 = cb.equal(c.get("makeId"),ocpm2.get("makeId"));
 			Predicate a9 = cb.equal(c.get("companyId"),ocpm2.get("companyId"));
 			Predicate a10 = cb.equal(c.get("branchCode"),ocpm2.get("branchCode"));
-			Predicate a12 = cb.equal(c.get("bodyId"),ocpm2.get("bodyId"));
-			effectiveDate2.where(a4,a5,a6,a9,a10,a12);
+			//Predicate a12 = cb.equal(c.get("bodyId"),ocpm2.get("bodyId"));
+			effectiveDate2.where(a4,a5,a6,a9,a10);
 			// Where
 			Predicate n1 = cb.equal(c.get("status"),"Y");
 			Predicate n9 = cb.equal(c.get("status"),"R");
@@ -919,16 +919,16 @@ public class MotorMakeModelMasterServiceImpl implements MotorMakeModelMasterServ
 			Predicate n8 = cb.equal(c.get("makeId"),req.getMakeId());
 			Predicate n14 = cb.equal(c.get("makeId"),"99999");
 			Predicate n15 = cb.or(n8,n14);
-			if(StringUtils.isNotBlank(req.getBodyId()) ) {
+			/*if(StringUtils.isNotBlank(req.getBodyId()) ) {
 				Predicate n11 = cb.equal(c.get("bodyId"),req.getBodyId() );
 				Predicate n12 = cb.equal(c.get("bodyId"),"99999");
 				Predicate n13 = cb.or(n11,n12);
 				
 				query.where(n10,n2,n3,n4,n7,n13,n15).orderBy(orderList);
 				
-			} else {
+			} else {*/
 				query.where(n10,n2,n3,n4,n7,n15).orderBy(orderList);
-			}
+		//	}
 			
 			// Get Result
 			TypedQuery<MotorMakeModelMaster> result = em.createQuery(query);
