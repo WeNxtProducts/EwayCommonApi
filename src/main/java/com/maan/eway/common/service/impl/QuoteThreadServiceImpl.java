7ed12b31ad25d576d777748b4ca7851d67699d18
Suganya <89253083+MaanRsa001@users.noreply.github.com>
2025-03-26 16:03:21 +0530

    Update QuoteThreadServiceImpl.java

--- src/main/java/com/maan/eway/common/service/impl/QuoteThreadServiceImpl.java
+++ src/main/java/com/maan/eway/common/service/impl/QuoteThreadServiceImpl.java
@@ -909,10 +909,11 @@ public class QuoteThreadServiceImpl implements QuoteThreadService {
 				// Update Non Opted Covers
 				for (FactorRateRequestDetails cover : nonOptCovers ) {
 					String userOptCond = StringUtils.isNotBlank(cover.getUserOpt()) && "Y".equalsIgnoreCase(cover.getUserOpt()) 
-							&& cover.getEndtCount()!=null && cover.getEndtCount().compareTo(BigDecimal.ZERO) >0 ? "Y" :"N";
+							&& cover.getEndtCount()!=null && cover.getEndtCount().compareTo(BigDecimal.ZERO) >=0 ? "Y" :"N";
 					cover.setUserOpt(userOptCond);
 					
 				}
+				
 				facRateRepo.saveAllAndFlush(nonOptCovers);
 				em.flush();
 			}	
