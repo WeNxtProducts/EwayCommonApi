c34982af83fa3109659a8c4833b30a3f76644ace
Amalwilliam <89253083+MaanRsa001@users.noreply.github.com>
2025-03-31 15:22:29 +0530

    fixed buypolicy useropt issue

--- src/main/java/com/maan/eway/common/service/impl/QuoteThreadServiceImpl.java
+++ src/main/java/com/maan/eway/common/service/impl/QuoteThreadServiceImpl.java
@@ -823,7 +823,7 @@ public class QuoteThreadServiceImpl implements QuoteThreadService {
 					for (CoverIdsReq cov : coverList) {
 						
 						if(StringUtils.isBlank(cov.getSubCoverYn()) || cov.getSubCoverYn().equalsIgnoreCase("N") ) {
-							List<FactorRateRequestDetails> filterCovers = covers.stream().filter( o -> o.getVehicleId().equals(veh.getVehicleId()) && o.getSectionId().equals(Integer.valueOf(veh.getSectionId())) &&  o.getCoverId().equals(cov.getCoverId()) ).collect(Collectors.toList());	
+							List<FactorRateRequestDetails> filterCovers = covers.stream().filter( o -> o.getLocationId().compareTo(veh.getLocationId())==0 &&   o.getVehicleId().equals(veh.getVehicleId()) && o.getSectionId().equals(Integer.valueOf(veh.getSectionId())) &&  o.getCoverId().equals(cov.getCoverId()) ).collect(Collectors.toList());	
 							userOptCovers.addAll(filterCovers);
 							
 							List<FactorRateRequestDetails> filterReferalCovers = filterCovers.stream().filter( o ->  o.getVehicleId().equals(veh.getVehicleId()) && o.getSectionId().equals(Integer.valueOf(veh.getSectionId())) && o.getCoverId().equals(cov.getCoverId()) &&  o.getDiscLoadId().equals(0) &&  o.getTaxId().equals(0) &&  o.getIsReferral()!=null && o.getIsReferral().equalsIgnoreCase("Y") ).collect(Collectors.toList());
@@ -839,7 +839,7 @@ public class QuoteThreadServiceImpl implements QuoteThreadService {
 							}
 						
 						} else {
-							List<FactorRateRequestDetails> filterSubCovers  = covers.stream().filter( o -> o.getVehicleId().equals(veh.getVehicleId()) &&  o.getSectionId().equals(Integer.valueOf(veh.getSectionId())) && o.getCoverId().equals(cov.getCoverId()) && o.getSubCoverId().equals(Integer.valueOf(cov.getSubCoverId()))  ).collect(Collectors.toList());
+							List<FactorRateRequestDetails> filterSubCovers  = covers.stream().filter( o -> o.getLocationId().compareTo(veh.getLocationId())==0 &&  o.getVehicleId().equals(veh.getVehicleId()) &&  o.getSectionId().equals(Integer.valueOf(veh.getSectionId())) && o.getCoverId().equals(cov.getCoverId()) && o.getSubCoverId().equals(Integer.valueOf(cov.getSubCoverId()))  ).collect(Collectors.toList());
 							userOptCovers.addAll(filterSubCovers);
 							List<FactorRateRequestDetails> filterReferalSubCovers = filterSubCovers.stream().filter( o ->  o.getVehicleId().equals(veh.getVehicleId()) &&  o.getCoverId().equals(cov.getCoverId()) &&  o.getDiscLoadId().equals(0) &&  o.getTaxId().equals(0) &&   o.getIsReferral()!=null && o.getIsReferral().equalsIgnoreCase("Y")  ).collect(Collectors.toList());
 							if(filterReferalSubCovers.size()>0  ) { 
